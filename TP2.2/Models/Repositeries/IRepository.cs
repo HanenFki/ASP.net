66a7dceee4ff50c0154c411bed54892bc7a38f1f
HanenFki <hanenfki8@gmail.com>
2024-05-28 00:45:17 +0200

    tp2

---
+++ TP2.2/Models/Repositeries/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace TP2._2.Models.Repositeries
+{
+    public interface IRepository<T>
+    {
+        T Get(int Id);
+        IEnumerable<T> GetAll();
+        T Add(T t);
+        T Update(T t);
+        T Delete(int Id);
+    }
+}
---
+++ TP2.2/Models/Repositeries/SqlProductRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace TP2._2.Models.Repositeries
+{
+    public class SqlProductRepository : IRepository<Product>
+    {
+        private readonly AppDBContext context;
+        public SqlProductRepository(AppDBContext context)
+        {
+            this.context = context;
+        }
+        public Product Add(Product P)
+        {
+            context.Products.Add(P);
+            context.SaveChanges();
+            return P;
+        }
+        public Product Delete(int Id)
+        {
+            Product P = context.Products.Find(Id);
+            if (P != null)
+            {
+                context.Products.Remove(P);
+                context.SaveChanges();
+            }
+            return P;
+        }
+        public IEnumerable<Product> GetAll()
+        {
+            return context.Products;
+        }
+        public Product Get(int Id)
+        {
+            return context.Products.Find(Id);
+        }
+        public Product Update(Product P)
+        {
+            var Product =
+            context.Products.Attach(P);
+            Product.State = EntityState.Modified;
+            context.SaveChanges();
+            return P;
+        }
+    }
+}
+
+
